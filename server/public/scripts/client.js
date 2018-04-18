c3c6332ed817028a1a53372bff4f0518e8d12eee
Torben Jepsen <torbenjepsen@outlook.com>
2018-04-18 16:14:43 -0500

    added array to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,24 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery is loaded');
+    $('#makeQuoteButton').on('click', function () {
+        $.ajax({
+            type: 'GET',
+            url: '/quote'
+        })
+            .then(function (response) {
+                console.log(response);
+                $('#quote').empty();
+                $('#quote').append('<p>' + response.quote + ' - ' + response.author + '</p');
+            });
+    });
+    $.ajax({
+        type: 'GET',
+        url: '/all-quotes'
+    })
+        .then(function (response) {
+            for (let i = 0; i < response.length; i++) {
+                $('#allQuotes').append('<ul><li>' + response[i].quote + ' - ' + response[i].author + '</li></ul>');
+            }
+        });
 }
